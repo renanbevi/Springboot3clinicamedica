90172ca14967f69ffa304bf32f832629917843b7
renanbevi <renanbevi@gmail.com>
2023-01-23 18:14:00 -0300

    Adicionei a atualização dos dados dos médicos no código.

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -2,16 +2,11 @@ package med.voll.api.controller;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
-import med.voll.api.endereco.Endereco;
-import med.voll.api.medico.DadosCadastroMedico;
-import med.voll.api.medico.Medico;
-import med.voll.api.medico.MedicoRepository;
+import med.voll.api.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController //essa classe é um controller Rest carregue a classe
 @RequestMapping("medicos")
@@ -34,6 +29,12 @@ public class MedicoController {
       // Oageable classe do Spring para paginação na hora de listar todos os itens
       // Page devolve informações sobre paginação com a lista dos dados dos médicos.
 
+    @PutMapping
+    @Transactional
+    public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados){
+        var medico = repository.getReferenceById(dados.id());  //busca a referencia do ID
+        medico.atualizarInformacoes(dados); // carreguei os dados do medico e vou atualizar com as informações atual
 
+    }
 
 }
--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -43,4 +43,16 @@ public class Medico {
         this.endereco = new Endereco(dados.endereco());
         this.telefone = dados.telefone();
     }
+
+    public void atualizarInformacoes(DadosAtualizacaoMedico dados) {
+        if(dados.nome() != null){  // campo não obrigatorio if para que se o item for null não atualizar o nome para "Null".
+            this.nome = dados.nome();
+        }
+        if(dados.telefone() != null){
+            this.telefone = dados.telefone();
+        }
+        if(dados.endereco() != null){
+            this.endereco.atualizarInformacoes(dados.endereco());
+        }
+    }
 }
