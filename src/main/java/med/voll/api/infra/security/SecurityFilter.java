8dd56b5f17af1f890ce726c61d6b2ff9110081dc
renanbevi <renanbevi@gmail.com>
2023-02-18 15:11:03 -0300

    Liberar e restringir requisições, de acordo com a URL e o verbo do protocolo HTTP.

--- src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -5,7 +5,11 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import med.voll.api.domain.usuario.Usuario;
+import med.voll.api.domain.usuario.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -15,30 +19,41 @@ import java.io.IOException;
 public class SecurityFilter  extends OncePerRequestFilter { //Herdando uma classe do Spring para filtro
 
     @Autowired
-    private TokenService  tokenService;
+    private TokenService tokenService;
+
+    @Autowired
+    private UsuarioRepository respository;
 
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+
         //lógica de recuperar o token
         var tokenJWT = recuperarToken(request);
 
-        //chegar se o token está válido e recuperar o Subject do usuário e senha
-        var subject = tokenService.getSubject(tokenJWT);
-       
+        if (tokenJWT != null) {
+            //chegar se o token está válido e recuperar o Subject do usuário e senha
+            var subject = tokenService.getSubject(tokenJWT); //recupera o token vindo no cabeçalho
+            var usuario = respository.findByLogin(subject);//o token ta valido considera-se que ela está logada spring
 
-        //filterchain representa a cadeia de filtros da aplicação
-        filterChain.doFilter(request, response);  //seguir o fluxo da requisição
-
-    }
-
-    private String recuperarToken(HttpServletRequest request) {
-        var authorizationHeader = request.getHeader("Authorization");  //getHeader pega o cabeçalho
-        if (authorizationHeader == null) { //aqui informa que o cabeçalho do header no postman veio vazio não tem token
-            throw new RuntimeException("Token JWT não enviado no cabeçalho authorization Header");
+            var authentication =  new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication); //SecurityContextHolder
 
         }
 
-        return authorizationHeader.replace("Bearer ", ""); // apagará o prefixo Bearer no token na impressão
+        //filterchain representa a cadeia de filtros da aplicação
+        filterChain.doFilter(request, response);  //seguir o fluxo da requisição
     }
+
+
+    private String recuperarToken(HttpServletRequest request) {
+        var authorizationHeader = request.getHeader("Authorization");  //getHeader pega o cabeçalho
+        if (authorizationHeader != null) {
+
+            return authorizationHeader.replace("Bearer ", ""); // apagará o prefixo Bearer no token na impressão
+        }
+        return null;
+    }
+
 }
